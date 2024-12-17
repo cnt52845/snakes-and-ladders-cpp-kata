bb68a3996760745f764549dab57cf66ef10d6f7f
skoterev <skoterev@ford.com>
2024-12-17 12:19:47 -0500

    Refactoring - create test fixture, delete PlayersImpl::CanCreateInstance

--- test_players_impl.cpp
+++ test_players_impl.cpp
@@ -1,13 +1,12 @@
 #include <gtest/gtest.h>
 #include "players_impl.h"
 
-TEST(PlayersImpl, CanCreateInstance)
-{
+class PlayersImplTest : public ::testing::Test {
+public:
     PlayersImpl players;
-}
+};
 
-TEST(PlayersImpl, Given_getName_called_When_no_players_added_Then_return_empty_string)
+TEST_F(PlayersImplTest, Given_getName_called_When_no_players_added_Then_return_empty_string)
 {
-    PlayersImpl players;
     ASSERT_EQ("", players.getName());
 }
