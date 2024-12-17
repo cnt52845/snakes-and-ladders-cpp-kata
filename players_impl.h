5b7926be7ae6c8ddb4a7f6782eafd688170e552e
skoterev <skoterev@ford.com>
2024-12-17 17:48:38 -0500

    PlayersImpl::Given_getName_called_When_one_player_added_Then_return_name - pass

--- players_impl.h
+++ players_impl.h
@@ -4,9 +4,12 @@
 
 class PlayersImpl : public Players {
 public:
-    std::string getName() const override { return ""; }
+    std::string getName() const override { return name; }
     int         getPosition() const override { return 0; }
     void        setPosition(int) override {}
     void        nextPlayer() override {}
-    void        addPlayer(std::string name) {}
+    void        addPlayer(std::string name) { this->name = name; }
+
+private:
+    std::string name{""};
 };
