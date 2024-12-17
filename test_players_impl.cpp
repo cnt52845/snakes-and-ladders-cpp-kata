5b39ee0e971ebc4a1f012db1ee0c0257bd74cb51
skoterev <skoterev@ford.com>
2024-12-17 12:14:22 -0500

    PlayersImpl::Given_getName_called_When_no_players_added_Then_return_empty_string

--- test_players_impl.cpp
+++ test_players_impl.cpp
@@ -5,3 +5,9 @@ TEST(PlayersImpl, CanCreateInstance)
 {
     PlayersImpl players;
 }
+
+TEST(PlayersImpl, Given_getName_called_When_no_players_added_Then_return_empty_string)
+{
+    PlayersImpl players;
+    ASSERT_EQ("", players.getName());
+}
