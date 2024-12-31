0c4afead8f7688b4a99f42bc6b4ad637f9322439
skoterev <skoterev@ford.com>
2024-12-31 16:40:46 -0500

    TestPlayersImplMultiplePlayers::Given_getName_called_When_multiple_players_added_Then_return_first_player_name - pass

--- players_impl.h
+++ players_impl.h
@@ -4,26 +4,39 @@
 
 class PlayersImpl : public Players {
 public:
-    std::string getName() const override { return player.name; }
-    int         getPosition() const override { return player.position; }
-    void        setPosition(int position) override
+    std::string getName() const override
     {
-        if (isPlayerAdded) {
-            player.position = position;
+        if (!players.size()) {
+            return "";
         }
+        return players[currentPlayerIndex].name;
     }
-    void nextPlayer() override {}
-    void addPlayer(std::string name)
+
+    int getPosition() const override
     {
-        player.name   = name;
-        isPlayerAdded = true;
+        if (!players.size()) {
+            return 0;
+        }
+        return players[currentPlayerIndex].position;
     }
+
+    void setPosition(int position) override
+    {
+        if (!players.size()) {
+            return;
+        }
+        players[currentPlayerIndex].position = position;
+    }
+
+    void nextPlayer() override {}
+
+    void addPlayer(std::string name) { players.emplace_back(Player{name, 0}); }
 
 private:
     struct Player {
         std::string name{""};
         int         position{0};
     };
-    Player player;
-    bool   isPlayerAdded{false};
+    std::vector<Player> players;
+    std::size_t         currentPlayerIndex{0};
 };
