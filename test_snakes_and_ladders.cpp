f32c8f5bdcb2c03bd312979c54b19ba5e9303899
skoterev <skoterev@ford.com>
2025-01-03 01:21:51 -0500

    TestSnakesAndLaders::CanPlay

--- test_snakes_and_ladders.cpp
+++ test_snakes_and_ladders.cpp
@@ -13,3 +13,14 @@ TEST(TestSnakesAndLaders, CanCreateInstance)
     std::shared_ptr<SnakesAndLadders> game =
         std::make_shared<SnakesAndLadders>(players, board, messenger);
 }
+
+TEST(TestSnakesAndLaders, CanPlay)
+{
+    std::shared_ptr<PlayersMock>      players   = std::make_shared<PlayersMock>();
+    std::shared_ptr<BoardMock>        board     = std::make_shared<BoardMock>();
+    std::shared_ptr<MessengerMock>    messenger = std::make_shared<MessengerMock>();
+    std::shared_ptr<SnakesAndLadders> game =
+        std::make_shared<SnakesAndLadders>(players, board, messenger);
+
+    game->play(2, 3);
+}
