da3c8fa651f2992a8ec3dd6c48039ee9b7e927b1
skoterev <skoterev@ford.com>
2025-01-03 01:06:59 -0500

    TestSnakesAndLaders::CanCreateInstance - pass

--- snakes_and_ladders.h
+++ snakes_and_ladders.h
@@ -1,5 +1,14 @@
 #pragma once
 
-class SnakesAndLaders {
+#include <memory>
+#include "players.h"
+#include "board.h"
+#include "messenger.h"
+
+class SnakesAndLadders {
 public:
+    SnakesAndLadders(std::shared_ptr<Players> players, std::shared_ptr<Board> board,
+                     std::shared_ptr<Messenger> messenger)
+    {
+    }
 };
