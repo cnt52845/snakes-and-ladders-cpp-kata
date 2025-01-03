79f8d3eb54764f02429f6c602fe22bdb4e72d10c
skoterev <skoterev@ford.com>
2025-01-02 20:02:17 -0500

    TestBoardImpl::Given_move_called_When_steps_0_Then_return_original_position

--- test_board_impl.cpp
+++ test_board_impl.cpp
@@ -5,3 +5,9 @@ TEST(TestBoardImpl, CanCreateInstance)
 {
     BoardImpl board;
 }
+
+TEST(TestBoardImpl, Given_move_called_When_steps_0_Then_return_original_position)
+{
+    BoardImpl board;
+    ASSERT_EQ(0, board.move(0, 0));
+}
