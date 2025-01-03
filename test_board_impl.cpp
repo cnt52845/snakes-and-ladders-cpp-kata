8e5f714317d821e0781a62798de3e06a212de3b4
skoterev <skoterev@ford.com>
2025-01-02 20:08:26 -0500

    Refactoring: create test fixture, delete TestBoardImpl::CanCreateInstance

--- test_board_impl.cpp
+++ test_board_impl.cpp
@@ -1,13 +1,12 @@
 #include <gtest/gtest.h>
 #include "board_impl.h"
 
-TEST(TestBoardImpl, CanCreateInstance)
-{
+class TestBoardImpl : public ::testing::Test {
+public:
     BoardImpl board;
-}
+};
 
-TEST(TestBoardImpl, Given_move_called_When_steps_0_Then_return_original_position)
+TEST_F(TestBoardImpl, Given_move_called_When_steps_0_Then_return_original_position)
 {
-    BoardImpl board;
     ASSERT_EQ(0, board.move(0, 0));
 }
