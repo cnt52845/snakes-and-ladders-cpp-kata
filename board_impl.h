1fdc21962aac90a4d73bd8ba9a31ccc51fcd2bb0
skoterev <skoterev@ford.com>
2025-01-02 20:41:55 -0500

    Refactoring: BoardImpl use map to define snakes and ladders

--- board_impl.h
+++ board_impl.h
@@ -1,18 +1,20 @@
 #pragma once
 
+#include <unordered_map>
 #include "board.h"
 
 class BoardImpl : public Board {
 public:
     int move(int fromPosition, int steps) const override
     {
-        int newPosition = fromPosition + steps;
-        if (newPosition == 2) {
-            newPosition = 38;
-        }
-        else if (newPosition == 7) {
-            newPosition = 14;
+        int  newPosition = fromPosition + steps;
+        auto it          = snakesAndLadders.find(newPosition);
+        if (it != snakesAndLadders.end()) {
+            newPosition = it->second;
         }
         return newPosition;
     }
+
+private:
+    std::unordered_map<int, int> snakesAndLadders{{2, 38}, {7, 14}};
 };
