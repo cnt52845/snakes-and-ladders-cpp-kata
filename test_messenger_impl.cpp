21d68258321d4df5c9c0e4b69d32a8641bbe4b05
skoterev <skoterev@ford.com>
2025-01-02 23:13:20 -0500

    Refactoring: create test fixture, remove TestMessengerImpl::CanCreateInstance

--- test_messenger_impl.cpp
+++ test_messenger_impl.cpp
@@ -2,14 +2,14 @@
 
 #include "messenger_impl.h"
 
-TEST(TestMessengerImpl, CanCreateInstance)
-{
+class TestMessengerImpl : public ::testing::Test {
+public:
     MessengerImpl messenger;
-}
+};
 
-TEST(TestMessengerImpl,
-     Given_playerPosition_called_When_playerName_is_1_and_position_is_5_Then_return_player_position)
+TEST_F(
+    TestMessengerImpl,
+    Given_playerPosition_called_When_playerName_is_1_and_position_is_5_Then_return_player_position)
 {
-    MessengerImpl messenger;
     ASSERT_EQ("Player 1 is on square 5", messenger.playerPosition("1", 5));
 }
