9ea8c2cf16ff7b24dd67fb38c3beee7d15b1e531
skoterev <skoterev@ford.com>
2025-01-02 20:21:40 -0500

    TestBoardImpl::Given_move_called_When_landed_on_2_Then_return_38

--- test_board_impl.cpp
+++ test_board_impl.cpp
@@ -15,3 +15,8 @@ TEST_F(TestBoardImpl, Given_move_called_When_steps_1_Then_return_position_plus_s
 {
     ASSERT_EQ(1, board.move(0, 1));
 }
+
+TEST_F(TestBoardImpl, Given_move_called_When_landed_on_2_Then_return_38)
+{
+    ASSERT_EQ(38, board.move(2, 0));
+}
