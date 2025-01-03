28026b58690a94da311e5dfc8771b2e6a6d9238f
skoterev <skoterev@ford.com>
2025-01-02 21:18:58 -0500

    MoveTests::TestBoardImpl - add all snakes and ladders

--- test_board_impl.cpp
+++ test_board_impl.cpp
@@ -28,9 +28,18 @@ TEST_P(TestBoardImpl, Move)
     ASSERT_EQ(params.newPosition, board.move(params.fromPosition, params.steps));
 }
 
-INSTANTIATE_TEST_SUITE_P(MoveTests, TestBoardImpl,
-                         ::testing::Values(
-                             // Regular moves
-                             MoveTestParams{0, 0, 0}, MoveTestParams{0, 1, 1},
-                             // Snakes
-                             MoveTestParams{2, 0, 38}, MoveTestParams{7, 0, 14}));
+INSTANTIATE_TEST_SUITE_P(
+    MoveTests, TestBoardImpl,
+    ::testing::Values(
+        // Regular moves
+        MoveTestParams{0, 0, 0}, MoveTestParams{0, 1, 1},
+        // Ladders
+        MoveTestParams{2, 0, 38}, MoveTestParams{7, 0, 14}, MoveTestParams{8, 0, 31},
+        MoveTestParams{15, 0, 26}, MoveTestParams{21, 0, 42}, MoveTestParams{28, 0, 84},
+        MoveTestParams{36, 0, 44}, MoveTestParams{51, 0, 67}, MoveTestParams{71, 0, 91},
+        MoveTestParams{78, 0, 98}, MoveTestParams{87, 0, 94},
+        // Snakes
+        MoveTestParams{16, 0, 6}, MoveTestParams{46, 0, 25}, MoveTestParams{49, 0, 11},
+        MoveTestParams{62, 0, 19}, MoveTestParams{64, 0, 60}, MoveTestParams{74, 0, 53},
+        MoveTestParams{89, 0, 68}, MoveTestParams{92, 0, 88}, MoveTestParams{95, 0, 75},
+        MoveTestParams{99, 0, 80}));
