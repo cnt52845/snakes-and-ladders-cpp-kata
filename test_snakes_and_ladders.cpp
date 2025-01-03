59c6f4f2e4f1c4db6898658172f970e9f0a66646
skoterev <skoterev@ford.com>
2025-01-03 01:06:29 -0500

    TestSnakesAndLaders::CanCreateInstance

--- test_snakes_and_ladders.cpp
+++ test_snakes_and_ladders.cpp
@@ -1,8 +1,15 @@
-#include "snakes_and_ladders.h"
-
+#include <memory>
 #include <gtest/gtest.h>
+#include "snakes_and_ladders.h"
+#include "players_mock.h"
+#include "board_mock.h"
+#include "messenger_mock.h"
 
-TEST(SnakesAndLaders, Dummy)
+TEST(TestSnakesAndLaders, CanCreateInstance)
 {
-    SUCCEED();
+    std::shared_ptr<PlayersMock>      players   = std::make_shared<PlayersMock>();
+    std::shared_ptr<BoardMock>        board     = std::make_shared<BoardMock>();
+    std::shared_ptr<MessengerMock>    messenger = std::make_shared<MessengerMock>();
+    std::shared_ptr<SnakesAndLadders> game =
+        std::make_shared<SnakesAndLadders>(players, board, messenger);
 }
