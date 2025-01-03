d5e26907a9d93ac6ae6f11da119fe33275f93d33
skoterev <skoterev@ford.com>
2025-01-02 23:07:52 -0500

    TestMessengerImpl::Given_playerPosition_called_When_playerName_is_1_and_position_is_5_Then_return_player_position - pass

--- messenger.h
+++ messenger.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+
 class Messenger {
 public:
+    virtual std::string playerPosition(std::string playerName, int position) const = 0;
 };
--- messenger_impl.h
+++ messenger_impl.h
@@ -4,4 +4,8 @@
 
 class MessengerImpl : public Messenger {
 public:
+    std::string playerPosition(std::string playerName, int position) const override
+    {
+        return "Player " + playerName + " is on square " + std::to_string(position);
+    }
 };
